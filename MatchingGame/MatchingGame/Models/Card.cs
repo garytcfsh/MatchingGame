7516067cdcacc3102cbfbadfce16ae10f12da432
toothpick <garytcfsh@gmail.com>
2024-01-09 00:34:25 +0800

    ListView

--- MatchingGame/MatchingGame/Models/Card.cs
+++ MatchingGame/MatchingGame/Models/Card.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
@@ -16,5 +17,8 @@ namespace MatchingGame.Models
 
         [ObservableProperty]
         private string _image;
+
+        [ObservableProperty]
+        private Visibility _visible = Visibility.Hidden;
     }
 }
--- MatchingGame/MatchingGame/ViewModels/MatchingViewModel.cs
+++ MatchingGame/MatchingGame/ViewModels/MatchingViewModel.cs
@@ -8,17 +8,56 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace MatchingGame.ViewModels
 {
     public partial class MatchingViewModel : ObservableObject
     {
+        private Card PreviousCard = null;
+
+        [ObservableProperty]
+        private bool _isEnabled = true;
+
         [ObservableProperty]
         private ObservableCollection<Card> _cards = new ObservableCollection<Card>();
 
         [ObservableProperty]
-        private string _hint = "";
+        private Card _selectedCard = new Card();
+        partial void OnSelectedCardChanged(Card? oldValue, Card newValue)
+        {
+            Debug.WriteLine($"{oldValue?.MatchingId} {newValue?.MatchingId}");
+            if (newValue != null)
+            {
+                newValue.Visible = System.Windows.Visibility.Visible;
+            }
+
+            if (PreviousCard != null && newValue != null)
+            {
+                Card preCard = PreviousCard;
+                IsEnabled = false;
+                Task.Run(() =>
+                {
+                    SpinWait.SpinUntil(()=>false, 1000);
+
+                    Application.Current.Dispatcher.Invoke(() => { 
+                        if (preCard != null)
+                        {
+                            preCard.Visible = System.Windows.Visibility.Hidden;
+                            preCard = null;
+                        }
+                        if (SelectedCard != null)
+                        {
+                            SelectedCard.Visible = System.Windows.Visibility.Hidden;
+                            SelectedCard = null;
+                        }
+                        IsEnabled = true;
+                    });
+                });
+            }
+            PreviousCard = newValue;
+        }
 
         public MatchingViewModel()
         {
@@ -30,8 +69,6 @@ namespace MatchingGame.ViewModels
                 card.Image = Path.GetFullPath(path);
                 Cards.Add(card);
             }
-
-            Hint = $"AFAFA {Cards.Count()}";
         }
     }
 }
